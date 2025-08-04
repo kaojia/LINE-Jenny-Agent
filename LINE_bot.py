c6ea83106ae74a8f7064b5d773681e1c19b057b1
kaojia <65601806+kaojia@users.noreply.github.com>
2025-08-04 22:03:07 +0800

    Update LINE_bot.py

--- LINE_bot.py
+++ LINE_bot.py
@@ -2,6 +2,11 @@ from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+import re
+from difflib import SequenceMatcher
 
 app = Flask(__name__)
 
